72be4372d60f531287ff3115b6d95fefdb23e45e
purshottamxkumar <purshottamk0011@gmail.com>
2023-03-13 22:31:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import searchGlass from "./images/search.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="main-heading">SNAPSHOT</h1>
+      <div className="middle-section">
+        <div className="search-center">
+          <input type="text" name="search-field" id="search-field" placeholder="Search..." />
+          <button type="text" name="search-btn" id="search-btn"><img src={searchGlass} alt="" id="search-btn-img" /></button>
+        </div>
+        <div className="option-btn-section">
+          <button id="option-btn" className="modified-btn">Mountain</button>
+          <button id="option-btn" className="modified-btn">Beaches</button>
+          <button id="option-btn" className="modified-btn">Birds</button>
+          <button id="option-btn" className="modified-btn">Food</button>
+        </div>
+      </div>
     </div>
   );
 }
